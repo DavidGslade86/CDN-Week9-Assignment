53ffe6badbcf4ac48174075155ce12085ef163a9
David Greenslade <david.greenslade.cohort2211@gmail.com>
2024-01-18 20:52:18 -0500

    added controllers for list routes

--- controllers/lists.js
+++ controllers/lists.js
@@ -6,8 +6,17 @@ const getAllLists = async (req, res) => {
     const lists = await List.find({createdBy: req.user.userId}).sort('createdAt');
     res.status(StatusCodes.OK).json({lists, count: lists.length});
 }
+
 const getList = async (req, res) => {
-    res.send('get task');
+    const {user:{userId}, params: {id: listId}} = req; 
+    const list = await List.find({
+        createdBy: userId, 
+        _id: listId,
+    });
+    if(!list) {
+        throw new NotFoundError(`No list with id ${listId} was found.`)
+    }
+    res.status(StatusCodes.OK).json({ list });
 }
 
 const createList = async (req, res) => {
@@ -17,10 +26,50 @@ const createList = async (req, res) => {
 }
 
 const updateList = async (req, res) => {
-    res.send('update task');
+    const {
+        body,
+        user:{userId},
+        params: {id: listId}
+    } = req;
+
+    if(Object.keys(body).length === 0) {
+        throw new BadRequestError(`At least one field must be changed for update to occur.`)
+    }
+
+    const list = await List.findByIdAndUpdate(
+        {
+            createdBy: userId, 
+            _id: listId,
+        },
+        req.body,
+        {
+            new:true,
+            runValidators:true
+        },
+    );
+
+    if(!list) {
+        throw new NotFoundError(`No task with id ${listId} was found.`)
+    }
+    res.status(StatusCodes.OK).json({success:true, data: list});
 }
+
 const deleteList = async (req, res) => {
-    res.send('delete task');
+    const {
+        user:{userId},
+        params: {id: listId}
+    } = req; 
+
+    const list = await List.findOneAndRemove({
+        createdBy: userId, 
+        _id: listId,
+    });
+
+    if(!list) {
+        throw new NotFoundError(`No task with id ${listId} was found.`)
+    }
+
+    res.status(StatusCodes.OK).json({success:true, message:`List with id ${listId} was removed`});
 }
 
 module.exports = {
